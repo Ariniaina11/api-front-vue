07fbf36d3d6922c1615e0c9f90d8f2e7b82d9566
Ariniaina11 <rabesonmamitiana@gmail.com>
2024-03-13 10:55:52 +0300

    add compoents

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam iusto, officia laborum nulla similique tempore aut asperiores nobis accusantium inventore, illo, rerum ab error reiciendis iure maiores. Repellendus, aspernatur animi.</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'MainComponent'
+    }
+</script>
+
+<style>
+    
+</style>
