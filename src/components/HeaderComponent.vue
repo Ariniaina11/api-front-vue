0e466aa54e218059f5bff9b5f821be0f5c4d35a1
Ariniaina11 <rabesonmamitiana@gmail.com>
2024-03-27 12:15:21 +0300

    Add font awesome

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,7 +1,13 @@
 <template>
-    <div class="flex bg-teal-800 text-white m-auto mx-3 mt-2 p-3 ">
-        <h1 class="my-auto text-2xl">{{ title }}</h1>
-        <button type="button" class="logout-btn mx-">Logout</button>
+    <div class="bg-teal-800 text-white m-auto mx-3 mt-2 p-3 ">
+        <div class="my-auto inline">
+            <font-awesome-icon icon="user-secret" class="text-3xl mr-2 text-gray-200"></font-awesome-icon>
+            <h1 class="text-2xl inline">{{ title }}</h1>
+        </div>
+        <div class="float-right">
+            <button type="button" class="font-serif text-gray-300 mr-5 px-4 py-1 border-solid border border-gray-300 rounded text-lg hover:text-gray-700 hover:bg-gray-300 hover:ease-in hover:duration-200">Sign In</button>
+            <button type="button" class="font-serif text-gray-700 mr-5 px-4 py-1 border-solid border border-gray-300 rounded text-lg bg-gray-300">Sign Up</button>
+        </div>
     </div>
 </template>
 
@@ -13,25 +19,3 @@
         }
     }
 </script>
-
-<style scoped>
-    .title{
-        /* font-size: 22px;
-        font-family: Verdana, Geneva, Tahoma, sans-serif;
-        width: 100%;
-        margin: auto; */
-    }
-    .logout-btn{
-        padding: 10px 20px;
-        border-radius: 2px;
-        border: 1px solid rgb(194, 194, 194);
-        font-family: inherit;
-        color: rgb(194, 194, 194);
-        font-weight: bold;
-    }
-    .logout-btn:hover{
-        color: white;
-        border-color: white;
-        transition: .5s;
-    }
-</style>
